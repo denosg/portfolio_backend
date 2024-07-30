0b188ed3df17b16d9f7cc5b3fee74695124ff51f
denos <costelasdenis@gmail.com>
2024-07-30 14:26:08 +0300

    changed download path

--- models/downloads.model.js
+++ models/downloads.model.js
@@ -4,12 +4,12 @@ const path = require('path');
  * Retrieves the file path for the PDF to be downloaded.
  * @returns {string} The file path for the PDF.
  */
-function getDownloadPdfPath()  {
+function getDownloadPdfPath() {
     const parentDir = path.resolve(__dirname, '..');
 
     const filePath = path.join(parentDir, 'public', 'cv_costelas_denis.pdf');
     return filePath;
 }
 module.exports = {
-    getDownloadPdfPath: getDownloadPdfPath,
+    getDownloadPdfPath,
 }
